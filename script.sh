019ce33a0ceaf683f4fcc27a7de7c28a490a4625
Aarushi-iitm <22f1001128@gmail.com>
2023-09-01 14:51:53 +0000

    Add script.sh

---
+++ script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export GITHUB_USERNAME="Aarushi-iitm"  # Replace with your GitHub username
+export GITHUB_REPOSITORY="se2001-22f1001128"  # Replace <your_roll_number> with your actual roll number
+
